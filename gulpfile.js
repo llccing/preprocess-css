8afd112a4249574ca6b5f13a650f051617bdcbe5
= <lcf33123@foxmail.com>
2017-06-21 00:57:19 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass',function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle:'expanded'}).on('error',sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch',function () {
+  gulp.watch('./sass/**/*.scss',['sass'])
+});
+
+gulp.task('default',['sass:watch']);
